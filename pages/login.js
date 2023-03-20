49f3a2d696c05158035c89301bdf44688cb49703
sheyax <olusheyax@gmail.com>
2023-03-20 13:04:50 +0100

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -18,7 +18,7 @@ export default function Login() {
           username,
           password,
         },
-         headers: {
+         {
         'Content-Type': 'application/json'
       },
         {
